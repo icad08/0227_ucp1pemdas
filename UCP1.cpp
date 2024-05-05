244fd43551035503a574dbcab9813fc8188953d9
icad08 <irsyadrm08@gmail.com>
2024-05-05 18:06:15 +0700

    first commit

---
+++ UCP1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// 1. int bilangan, float nilai, string nama
+// 2. conditional statment digunakan untuk menjalankan code berdasarkan kondisi yang diberikan, seperti if, else dan else if
+// 3. struct adalah struktur yang digunakan untuk mendefinisikan tipe data baru yang dapat menyimpan jenis data yang berbeda
+// 4. prosedur menggunakan void untuk nama prosedur, sedangkan fungsi menggunakan tipe data untuk nama fungsi
+// 5. looping berguna untuk melakukan tidakan secara berulang tanpa harus menulis ulang 
+
+int main (){
+struct mahasiswa{
+    string nama;
+    int nilai;
+
+};
+void input(){
+
+    cout << "dinyatakan lulus: ";
+    cin >> lulus
+    cout << "dinyatakan gagal: ";
+    cin >> gagal;
+}
+
+
+}
+int main(){
+
+}
